70a830755df9af1cb4d57e2c7957f91298a02fec
HugooBessaa <39413828+HugooBessaa@users.noreply.github.com>
2019-02-16 19:05:27 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('message', message => {
     	message.channel.send('Faça igual ao Azalim, pegue nele enfie no cu! 😆😆');
     }
     if (message.content === '+avisoteste') {
-        const channel = client.channels.find('<#546052170785751051>', channelName)
+        const channel = client.channels.find('chat', channelName)
         channel.send('TESTANDOOOOOOOOOOOOOOOOOO')
     }
 });
