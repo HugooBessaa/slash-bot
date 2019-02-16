c2ecb0fba51df55b4fb97cb2ac422728d1dca917
HugooBessaa <39413828+HugooBessaa@users.noreply.github.com>
2019-02-16 18:43:49 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,13 +8,14 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === '+slash') {
     	message.reply('alô me chamou? ixi verdade..\n:green_heart: Servidores SlashMC :green_heart:\n:shinto_shrine: Factions Mystic MCPE:\n**IP:** jogar.redeslash.com\n**Porta:** 19132\n**Versão:** 1.1.5');
-  	}
-    if (message.content === 'O que fazer com um dominio?') {
-    	message.channel.send('Faça igual ao Azalim, pegue nele enfie no cu! 😆😆');
-        message.channel.send({embed: {
+                message.channel.send({embed: {
   color: 0x4bf442,
   description: "IP: jogar.redeslash.com\nPorta: 19132\nVersão: 1.1x"
 }});
+    }
+  	}
+    if (message.content === 'O que fazer com um dominio?') {
+    	message.channel.send('Faça igual ao Azalim, pegue nele enfie no cu! 😆😆');
     }
 });
 
