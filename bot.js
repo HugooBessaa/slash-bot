a33254c80128bb393a9d84ee733087c424d2d8b0
HugooBessaa <39413828+HugooBessaa@users.noreply.github.com>
2019-02-16 18:13:17 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,12 +8,6 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === '+slash') {
     	message.reply('alô me chamou? ixi verdade..\n:green_heart: Servidores SlashMC :green_heart:\n:shinto_shrine: Factions Mystic MCPE:\n**IP:** jogar.redeslash.com\n**Porta:** 19132\n**Versão:** 1.1.5\nCurtiu o bot?');
-         message.react("regional_indicator_p.id")
-         message.react("regional_indicator_r.id")
-         message.react("regional_indicator_o.id")
-         message.react("regional_indicator_n.id")
-         message.react("regional_indicator_t.id")
-         message.react("regional_indicator_o.id")
   	}
 });
 
